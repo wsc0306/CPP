4086da8a14ac38340ff61ef5458b8d10d932efa2
wsc0306 <you@example.com>
2026-03-17 17:49:43 +0900

    first commit

--- Week3/p97_1.cpp
+++ Week3/p97_1.cpp
@@ -1,6 +1,6 @@
 #include <iostream> // input output stream
 int main() 
-// 1-1
+// num 1
 {
     for(int i = 1; i <= 5; ++i) 
      {
@@ -13,17 +13,66 @@ int main()
          }
          std::cout << "\n";
      }
+    std::cout << "\n";
     
 //==============================
-// 1-2
+// num 2
 
 
     
     for(int i = 5; i >= 1; --i) 
-     {
-        std::cout << i;
-       for(int j = 5; j >= i; --j)
-       {}
+     { std::cout << "\n";
+       for(int j = 5; j >= 5 - (i - 1); --j)
+       {
+              std::cout << j;
+             
+       }
+       
      }
      std::cout << "\n";
+
+//==============================
+// num 3
+
+for(int i =1; i<= 5; ++i)
+{ std::cout << "\n";
+    for(int j = 1; j <=5 -i; ++j)
+    {
+        std::cout << " ";
+    }
+
+    for(int j = i; j >= i; --j)
+    {
+        for(j = i; j>0; --j)
+        {
+        std::cout << j;
+        }
+    }
+
 }
+std::cout << "\n";
+
+//==============================
+// num 4
+
+for(int i = 1; i<=5; ++i)
+{
+    std::cout << "\n";
+    for(int j = 5 - i; j >= 1; --j)
+    {
+        std::cout << " ";
+    }
+    
+    for(int j = i; j >= 1; --j)
+    {
+        std::cout << j;
+    }
+
+    for(int j = 2; j <= i; ++j)
+    {
+        std::cout << j;
+    }
+}
+
+
+    }
